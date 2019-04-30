f6be73f4120c51d1ccbb8da5843de6e9a63a7409
Micolucci <micoluccileandro@gmail.com>
2019-04-29 23:24:45 -0300

    primer commit x eclipse

--- src/main/TipoDePrenda.java
+++ src/main/TipoDePrenda.java
@@ -5,18 +5,6 @@ public enum TipoDePrenda {
 	
 	// Los tipos de prenda deben de ser instancias de clases y conocer su categoria
 	
-	
-	
-	ZAPATO = new TipoDePrenda(CALZADO),
-    REMERA = new TipoDePrenda(PARTE_SUPERIOR),
-    PANTALON = new TipoDePrenda(PARTE_INFERIOR);
-	
-	
-	// Los tipos de prenda deben de ser instancias de clases y conocer su categoria
-	//Son tipos de prenda: remera, blusa, pantalon, bermuda, enterito, zapato, gorro, anteojo, camisa_manga_larga, camisa_manga_corta, vestido, pollera
-	//ZAPATO(CALZADO), REMERA(PARTE_SUPERIOR), PANTALON(PARTE_INFERIOR);
-	  
-    
 	 Categoria categoria;
 	
 	//Constructor: 
@@ -24,8 +12,20 @@ public enum TipoDePrenda {
 		this.categoria=unaCategoria;
 	}
 	
+	//categorias(CALZADO, PARTE_SUPERIOR, PARTE_INFERIOR, ACCESORIOS)
+	
 	public Categoria categoria() {
 		return this.categoria;
 	}
+	
+	zapato = new TipoDePrenda(CALZADO);
+    remera = new TipoDePrenda(PARTE_SUPERIOR);
+    blusa = new TipoDePrenda(PARTE_SUPERIOR);
+    pantalon = new TipoDePrenda(PARTE_INFERIOR);
+    bermuda = new TipoDePrenda(PARTE_INFERIOR);
+	
+	
+	//Son tipos de prenda: remera, blusa, pantalon, bermuda, enterito, zapato, gorro, anteojo, camisa_manga_larga, camisa_manga_corta, vestido, pollera
+
 
 }
