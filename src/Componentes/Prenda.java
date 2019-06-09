fe394efe049a1b57f80787d9c8b719798da40a3a
Silvina Gomez Cheda <sil.gch@gmail.com>
2019-06-08 23:34:30 -0300

    Se Modifican validaciones en prenda para no repetir codigo.

--- src/Componentes/Prenda.java
+++ src/Componentes/Prenda.java
@@ -1,8 +1,6 @@
 
 package Componentes;
 
-import java.util.Objects;
-
 public class Prenda {
 
 	private String nombre;
@@ -18,7 +16,7 @@ public class Prenda {
     public Prenda(String nombre, TipoDePrenda tipo, Material material, Color colorPrincipal, Trama trama) {
     	
 
-    		if(Validaciones.realizarValidacionesCuatroParametros(tipo, material, colorPrincipal, trama)) {
+    		if(Validaciones.validarCreacionPrenda(nombre, tipo, material, colorPrincipal, trama)) {
 		    	this.nombre=nombre;
     			this.tipo = tipo;
 		        this.material = material;
@@ -30,7 +28,7 @@ public class Prenda {
 
     public Prenda(String nombre,TipoDePrenda tipo,  Material material, Color colorPrincipal, Color colorSecundario, Trama trama) {
         
-	    	if(Validaciones.realizarValidacionesCincoParametros(tipo, material, colorPrincipal, colorSecundario, trama)) {
+	    	if(Validaciones.validarCreacionPrenda(nombre, tipo, material, colorPrincipal, colorSecundario, trama)) {
 	    		this.nombre=nombre;
 	    		this.tipo = tipo;
 	    		this.material = material;
@@ -52,13 +50,27 @@ public class Prenda {
     
 
 
+//Lo unico que podriamos cambiar de una prenda es el nombre .
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+//ESTE METODO LO USAMOS PARA QUE NO DEVUELVA LA POSICION EN MEMORIA CUANDO QUEREMOS IMPRIMIR POR CONSOLA LA LISTA DE PRENDAS
+	public String toString() {
+		return this.getNombre();
+		}
+	
+	
 //Getter
+	public String getNombre() {
+		return nombre;
+	}
     public Color getColor() {
         return colorPrincipal;
     }
 
     public Color getColorSecundario() {
-        return colorSecundario;
+    	return colorSecundario;
     }
 
     public Material getMaterial() {
@@ -77,6 +89,9 @@ public class Prenda {
         return tipo.getCategoria();
     }
 
+    
+    
+    // Ver si vamos a usar los setter. 
 	public void setColorPrincipal(Color colorPrincipal) {
 		this.colorPrincipal = colorPrincipal;
 	}
@@ -97,18 +112,4 @@ public class Prenda {
 		this.trama = trama;
 	}
 
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public Color getColorPrincipal() {
-		return colorPrincipal;
-	}
-	public String toString() {
-		return this.getNombre();
-		}
 }
--- src/Componentes/Validaciones.java
+++ src/Componentes/Validaciones.java
@@ -2,30 +2,12 @@ package Componentes;
 
 public class Validaciones {
 
-	public static boolean realizarValidacionesCincoParametros(TipoDePrenda tipo,  Material material, Color colorPrincipal, Color colorSecundario, Trama trama) {
-		boolean check= true;
-		try {
-		
-		//SE IRIAN AGREGANDO DISTINTAS VALIDACIONES
-		constructorSinNullCincoParametros(tipo, material, colorPrincipal, colorSecundario, trama);
-		tipo.puedeSerDeMaterial(material);
-		colorPrincipal.esIgualA(colorSecundario);
-		
-		}
-		catch(IllegalArgumentException e) {
-			check = false;
-			
-		}
-		
-		return check;
-	}
-	
-	public static boolean realizarValidacionesCuatroParametros(TipoDePrenda tipo,  Material material, Color colorPrincipal, Trama trama) {
+	public static  boolean validarCreacionPrenda(String nombre,TipoDePrenda tipo,  Material material, Color colorPrincipal, Trama trama) {
 		boolean check= true;
 		try {
 		
 		//SE IRIAN AGREGANDO DISTINTAS VALIDACIONES	
-		constructorSinNullCuatroParametros(tipo,material,colorPrincipal,trama);
+		constructorSinNull(nombre,tipo,material,colorPrincipal,trama);
 		tipo.puedeSerDeMaterial(material);
 		
 		}
@@ -37,12 +19,19 @@ public class Validaciones {
 		return check;
 	}
 	
-    public static void constructorSinNullCuatroParametros(TipoDePrenda tipo, Material material, Color colorPrincipal, Trama trama) {
+	
+	public static  boolean validarCreacionPrenda(	String nombre, TipoDePrenda tipo,  Material material,
+											Color colorPrincipal, Color colorSecundario, Trama trama) {
+		return validarCreacionPrenda( nombre, tipo,  material,  colorPrincipal, trama);
+	}
+	
+    public static void constructorSinNull(String nombre, TipoDePrenda tipo, Material material, Color colorPrincipal, Trama trama) {
     	
 		boolean check= false;
 		try {
 
-		check= (tipo==null||
+		check= (nombre==null||
+				tipo==null||
 				material==null||
 				colorPrincipal==null||
 				trama==null);
@@ -57,25 +46,6 @@ public class Validaciones {
 		
 	}
 
-    public static void constructorSinNullCincoParametros(TipoDePrenda tipo, Material material, Color colorPrincipal, Color colorSecundario, Trama trama) {
-    	
-		boolean check= false;
-		try {
 
-		check= (tipo==null||
-				material==null||
-				colorPrincipal==null||
-				//RECORDAR QUE AUNQUE SE INICIE CON CINCO PARAMETROS
-				//colorSecundario PUEDE SER NULL
-				trama==null);
-			if(check) {
-	        	throw new IllegalArgumentException("Todos los parametros del constructor deben ser distintos de null");
-	        }
-		}
-		catch(IllegalArgumentException e) {
-			System.out.println(e);
-			
-		}
-		
-	}
+	
 }
