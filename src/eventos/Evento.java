397dc0af7c3dc1be49b5af2994360af46dd29458
Silvina Gomez Cheda <sil.gch@gmail.com>
2019-09-03 18:28:13 -0300

    Se agregan eventos repetitivos.

--- src/eventos/Evento.java
+++ src/eventos/Evento.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.Period;
 
 import guardarropas.Guardarropa;
 import usuario.Usuario;
@@ -18,6 +19,8 @@ public class Evento {
 	private Double temperatura;
 	private List<Sugerencia> sugerencias = new ArrayList<Sugerencia>();
 	private ColaDeEventos servidorDeEventos = new ColaDeEventos();
+	private String repeticion;
+	private List<Evento> eventosConRepeticion;
 	
 	public ColaDeEventos getColaDeEventos() {
 		return servidorDeEventos;
@@ -29,15 +32,63 @@ public class Evento {
 		this.descripcion = descripcion;
 		this.usuario = usuario;
 		this.ubicacion = ubicacion;
+		this.repeticion = "UNICO";  //Averiguar como implementar calendario de google.
+	
 		
 	}
-	
+	public Evento CrearSiguienteEvento() throws Exception
+    {
+   
+        return new Evento(this.calcularSiguienteFecha(), this.descripcion, this.usuario, this.ubicacion);
+    }
+    
+    private LocalDate calcularSiguienteFecha() throws Exception {
+    	//Calcula el proximo evento segun su repeticion. 
+    	Period periodo;
+        switch (this.repeticion)
+        {
+        	case "UNICO": periodo = Period.ofDays(0);
+        	return fechaEvento.plus(periodo);
+        	
+            case "DIARIA": periodo = Period.ofDays(1);
+        	return fechaEvento.plus(periodo);
+          
+            case "SEMANAL":periodo = Period.ofDays(7);
+            	return fechaEvento.plus(periodo);
+        
+            
+            case "MENSUAL":periodo = Period.ofMonths(1);
+            	return fechaEvento.plus(periodo);
+           
+            case "ANUAL":periodo = Period.ofYears(1);
+            	
+            	 return fechaEvento.plus(periodo);
+         
+            	
+            default:
+                throw new Exception("Imposible calcular fecha del proximo evento");
+        }
+        
+        
+	}
 	//Agrego el evento a la cola
 	public void procesarEvento() throws IOException {
 		this.servidorDeEventos.encolarEvento(this);
 	}
 		
-	
+	private Boolean esUnaRepeticionValida( String repeticion) {
+        switch (repeticion)
+        {
+            case "UNICO":
+            case "DIARIA":
+            case "SEMANAL":
+            case "MENSUAL":
+            case "ANUAL":
+                return true;
+            default:
+                return false;
+        }
+    }
 	//Setter
 	public void setFechaEvento(LocalDate fechaEvento) {
 		this.fechaEvento = fechaEvento;
@@ -65,7 +116,15 @@ public class Evento {
 			sugerencias.add(auxiliar);
 		}
 	}
-	
+	public void setRepeticion(String repeticion) throws Exception {
+		
+		if(esUnaRepeticionValida(repeticion)){
+			this.repeticion = repeticion;
+			Evento nuevoEvento = this.CrearSiguienteEvento();
+			eventosConRepeticion.add(nuevoEvento);
+			
+		}
+	}
 	
 	//Getter
 	public LocalDate getFechaEvento() {
