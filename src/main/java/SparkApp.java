8181f8d3687cc96f4c390fe8af0baa6a245b02c3
Micolucci <micoluccileandro@gmail.com>
2019-12-09 19:26:10 -0300

    subiendolo a haroku

--- src/main/java/SparkApp.java
+++ src/main/java/SparkApp.java
@@ -187,8 +187,19 @@ public class SparkApp {
         return new VelocityTemplateEngine().render(
                 new ModelAndView(model, "templates/calendar.html")                     
         );
-    }); 
-   }  
+    });
+    
+   }
+  
+  static int getHerokuAssignedPort() {
+      ProcessBuilder processBuilder = new ProcessBuilder();
+      if (processBuilder.environment().get("PORT") != null) {
+          return Integer.parseInt(processBuilder.environment().get("PORT"));
+      }
+      return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+  }
+  
+  
 }
 
 /*Fachada fachada = new Fachada();
